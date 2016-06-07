72c265ee430c345f8d63f4d341e3a6e42af3c2f4
ponraj123 <ponraj.bluesky@gmail.com>
2016-06-07 14:32:04 +0530

    Update index.c

--- index.c
+++ index.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
-int main()
-{
-int i,n;
-int array[1000];
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-scanf("%d",array[i]);
-if(!(array[i]-i))
-printf("%d ",i);
-}
-return 0;
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	long int n,i;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++){
+	 scanf("%d",&arr[i]);
+	 if(i==arr[i])
+	 printf("%d equals to its index %d\n",arr[i],i);
+	}
+
+	return 0;
 }
